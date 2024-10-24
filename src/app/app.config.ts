b1d826b86aee85acb98315cc54c5ff46e994c2ec
Vitor Fel <vitorfel2000@gmail.com>
2024-10-24 20:49:24 -0300

    chore: Config biomejs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,14 @@
-import {
-  ApplicationConfig,
-  DEFAULT_CURRENCY_CODE,
-  LOCALE_ID,
-} from '@angular/core';
+import { type ApplicationConfig, DEFAULT_CURRENCY_CODE, LOCALE_ID } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
-import { APP_ROUTES } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { APP_ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(APP_ROUTES, withComponentInputBinding()),
-    provideHttpClient(),
-    { provide: LOCALE_ID, useValue: 'pt' },
-    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
-  ],
+	providers: [
+		provideRouter(APP_ROUTES, withComponentInputBinding()),
+		provideHttpClient(),
+		{ provide: LOCALE_ID, useValue: 'pt' },
+		{ provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+	],
 };
