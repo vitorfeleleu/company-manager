7fac53e44fcd10843af5078f5e76c7bbb8861fa8
Vitor Fel <vitorfel2000@gmail.com>
2024-10-27 00:32:10 -0300

    feat: Create component input mask

---
+++ src/app/shared/components/atoms/inputs/input-mask/input-mask.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ControlValueAccessorDirective } from '@shared/directives/control-value-accessor.directive';
+import { NgxMaskDirective } from 'ngx-mask';
+import { LabelInputMessageErrorComponent } from '../../label-input-message-error/label-input-message-error.component';
+
+@Component({
+  selector: 'guep-input-mask',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    LabelInputMessageErrorComponent,
+    NgxMaskDirective,
+  ],
+  templateUrl: './input-mask.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InputMaskComponent<T> extends ControlValueAccessorDirective<T> {
+  public maskInput = input.required<string>();
+}
