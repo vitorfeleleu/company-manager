619d5b0a6aa91cb70559ad6239f70153e2e0e75b
Vitor Fel <vitorfel2000@gmail.com>
2024-10-27 00:31:41 -0300

    feat: Create component label input message error

---
+++ src/app/shared/components/atoms/label-input-message-error/label-input-message-error.component.ts
@@ -0,0 +1,31 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from '@angular/core';
+import type { ValidationErrors } from '@angular/forms';
+
+@Component({
+  selector: 'guep-label-input-message-error',
+  standalone: true,
+  imports: [],
+  templateUrl: './label-input-message-error.component.html',
+  styleUrl: './label-input-message-error.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LabelInputMessageErrorComponent {
+  public errorsForm = input.required<Record<string, ValidationErrors> | null>();
+
+  protected errorTextScreen = computed<string | null>(() =>
+    this._getErrorText()
+  );
+
+  private _getErrorText(): string | null {
+    const erros = this.errorsForm();
+    if (!erros) return null;
+    if (erros.required) return 'Campo obrigatório';
+    if (erros.mask) return 'Formato incorreto';
+    return Object.getOwnPropertyNames(erros)[0];
+  }
+}
