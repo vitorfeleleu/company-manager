66310eedac75d94e1728db524d1d2180861747fc
Vitor Fel <vitorfel2000@gmail.com>
2024-10-27 19:27:59 -0300

    feat: Inserting footer component

--- src/app/shared/components/templates/main-layout/main-layout.component.ts
+++ src/app/shared/components/templates/main-layout/main-layout.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FooterComponent } from '@shared/components/molecules/footer/footer.component';
 import { SidebarMenuComponent } from '@shared/components/organisms/sidebar-menu/sidebar-menu.component';
 
 @Component({
   selector: 'guep-main-layout',
   standalone: true,
-  imports: [SidebarMenuComponent],
+  imports: [SidebarMenuComponent, FooterComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
