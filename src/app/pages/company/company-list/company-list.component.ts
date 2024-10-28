f9fbf0f9fbd2a0629f7c926b2a36ab88cc6d3cf2
Vitor Fel <vitorfel2000@gmail.com>
2024-10-27 23:12:11 -0300

    feat: Create component company list page

--- src/app/pages/company/company-list/company-list.component.ts
+++ src/app/pages/company/company-list/company-list.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { StandardPageComponent } from '@shared/components/organisms/standard-page/standard-page.component';
+import { CompanyListTemplateComponent } from '@shared/components/templates/company-list-template/company-list-template.component';
 
 @Component({
   selector: 'guep-company-list',
   standalone: true,
-  imports: [RouterLink],
+  imports: [StandardPageComponent, CompanyListTemplateComponent],
   templateUrl: './company-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
