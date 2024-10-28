b3439e362dea80e2fa3df2937282191403f8f22b
Vitor Fel <vitorfel2000@gmail.com>
2024-10-28 11:58:02 -0300

    feat: Entering Deletion Confirmation

--- src/app/shared/components/templates/company-list-template/company-list-template.component.ts
+++ src/app/shared/components/templates/company-list-template/company-list-template.component.ts
@@ -5,15 +5,17 @@ import {
   inject,
   signal,
 } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { TELEPHONE_MASK } from '@core/constant/masks';
 import { DeleteCompanyUseCase } from '@core/sql/use-cases/company/delete-company.use-case';
 import { ListCompanyUseCase } from '@core/sql/use-cases/company/list-company.use-case';
+import { ConfirmeDialogComponent } from '@shared/components/organisms/dialogs/confirme-dialog/confirme-dialog.component';
 import { TableComponent } from '@shared/components/organisms/table/table.component';
 import { ButtonDirective } from '@shared/directives/button.directive';
 import type { CompanyInterface } from '@shared/interfaces/company';
 import { CnpjPipe } from '@shared/pipes/cnpj.pipe';
 import { NgxMaskPipe } from 'ngx-mask';
+import { DialogService } from 'primeng/dynamicdialog';
 import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
@@ -25,6 +27,7 @@ import { TooltipModule } from 'primeng/tooltip';
     NgxMaskPipe,
     ButtonDirective,
     TooltipModule,
+    RouterLink,
   ],
   templateUrl: './company-list-template.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -33,6 +36,7 @@ export class CompanyListTemplateComponent implements OnInit {
   private _listCompanyUseCase = inject(ListCompanyUseCase);
   private _deleteCompanyUseCase = inject(DeleteCompanyUseCase);
   private _router = inject(Router);
+  private _dialog = inject(DialogService);
 
   protected companies = signal<CompanyInterface[]>([]);
   protected loadingTable = signal(true);
@@ -53,7 +57,21 @@ export class CompanyListTemplateComponent implements OnInit {
   }
 
   protected delete(id: number) {
-    this._deleteCompanyUseCase.execute(id);
-    this._getCompanies();
+    this._showConfirmDeletionDialog(id);
+  }
+
+  private _showConfirmDeletionDialog(id: number) {
+    const ref = this._dialog.open(ConfirmeDialogComponent, {
+      data: {
+        title: 'Confirmação de Exclusão',
+        description: 'Tem certeza de que deseja remover esta empresa?',
+      },
+    });
+    ref.onClose.subscribe((response) => {
+      if (response) {
+        this._deleteCompanyUseCase.execute(id);
+        this._getCompanies();
+      }
+    });
   }
 }
